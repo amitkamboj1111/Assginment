491063289ac36c15daf1007a0ebd7a719745f542
amitkamboj1111@gmail.com <amitkamboj1111@gmail.com>
2016-08-12 16:24:23 +0530

    jquery locally added and some UI fixes

--- index.php
+++ index.php
@@ -2,32 +2,32 @@
 <html>
 <head>
 <link rel="stylesheet" href="css/styles.css">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="css/jquery-3.1.0.min.js"></script>
 </head>
 <body>
 
 <section class="main">
-	 <form class="search" method="get" action="index.html" >
-		 <input autocomplete="off" id = "categoryId" data-id = "" type="text" name="q" placeholder="Search..." />
+	 <form class="search" method="get" >
+		 <input autocomplete="off" id = "categoryId" data-id = "" type="text" placeholder="Search..." />
 		 <ul class="results" >
-			<li id = "categoryId0" class ="categoryliclass" data-id = "" ><a href="index.html">Search Result #1</a></li>
-			<li id = "categoryId1" class ="categoryliclass" data-id = "" ><a href="index.html">Search Result #2</a></li>
-	 		<li id = "categoryId2" class ="categoryliclass" data-id = "" ><a href="index.html">Search Result #3</a></li>
-         	<li id = "categoryId3" class ="categoryliclass" data-id = "" ><a href="index.html">Search Result #4</a></li>
-         	<li id = "categoryId4" class ="categoryliclass" data-id = "" ><a href="index.html">Search Result #5</a></li>
+			<li id = "categoryId0" class ="categoryliclass" data-id = "" ><a href="index.html"></a></li>
+			<li id = "categoryId1" class ="categoryliclass" data-id = "" ><a href="index.html"></a></li>
+	 		<li id = "categoryId2" class ="categoryliclass" data-id = "" ><a href="index.html"></a></li>
+         	<li id = "categoryId3" class ="categoryliclass" data-id = "" ><a href="index.html"></a></li>
+         	<li id = "categoryId4" class ="categoryliclass" data-id = "" ><a href="index.html"></a></li>
 		 </ul>
 	 </form>
 </section>
 
 <section class="main">
-	 <form class="search" method="get" action="index.html" >
-		 <input autocomplete="off" id = "productId" data-id = "" type="text" name="q" placeholder="Search..." />
+	 <form class="search" method="get" >
+		 <input autocomplete="off" id = "productId" data-id = "" type="text" placeholder="Search..." />
 		 <ul class="results" >
-			<li id = "productId0" class ="productliclass" data-id = "" ><a href="index.html">Search Result #1</a></li>
-			<li id = "productId1" class ="productliclass" data-id = "" ><a href="index.html">Search Result #2</a></li>
-	 		<li id = "productId2" class ="productliclass" data-id = "" ><a href="index.html">Search Result #3</a></li>
-         	<li id = "productId3" class ="productliclass" data-id = "" ><a href="index.html">Search Result #4</a></li>
-         	<li id = "productId4" class ="productliclass" data-id = "" ><a href="index.html">Search Result #5</a></li>
+			<li id = "productId0" class ="productliclass" data-id = "" ><a href="index.html"></a></li>
+			<li id = "productId1" class ="productliclass" data-id = "" ><a href="index.html"></a></li>
+	 		<li id = "productId2" class ="productliclass" data-id = "" ><a href="index.html"></a></li>
+         	<li id = "productId3" class ="productliclass" data-id = "" ><a href="index.html"></a></li>
+         	<li id = "productId4" class ="productliclass" data-id = "" ><a href="index.html"></a></li>
 		 </ul>
 	 </form>
 </section>
@@ -38,12 +38,14 @@
     <span>Product Price:</span><span id = "productPrice"></span>
 </div>
 
-
 </body>
 <script type="text/javascript">
 
 $('#productInfo').hide();
-	var typingTimer;                //timer identifier
+$('.categoryliclass').hide();
+$('.productliclass').hide();
+
+var typingTimer;                //timer identifier
 var doneTypingInterval = 300;  //time in ms, 5 second for example
 var $input = $('#categoryId');
 
@@ -65,12 +67,10 @@ function hideOldResults() {
 }
 function showNewResults(results) {
 
-//alert(results);
 	results = $.parseJSON(results);
 
 	for (var i = 0; i < results.length; i++) {
 		if (results[i].hasOwnProperty('categoryId')) {
-			//alert(results[i]['categoryId']);
 			$('#categoryId'+i).data('id' , results[i]['categoryId']);
 			$('#categoryId'+i).text((results)[i]['categoryName']);
 			$('#categoryId'+i).show();
@@ -79,23 +79,21 @@ function showNewResults(results) {
 			break;
 	}
 }
-
 //user is "finished typing," do something
 function doneTyping () {
   $.ajax({url: "/backend/categories.php", 
 		data: {text : $('#categoryId').val()}, 
 		success: function(result){
-           // alert('here');
-        hideOldResults();
-        showNewResults(result);
+            hideOldResults();
+            showNewResults(result);
     }});
 }
 
 $('.categoryliclass').click(function(event) {
 	var text = $(event.target).data('id');
 	$('#categoryId').data('id',text);
-	//alert($('#categoryId').data('id'));
 	$('#categoryId').val($(event.target).text());
+    $('.categoryliclass').hide();
 });
 
 </script>
@@ -123,12 +121,10 @@ function hideOldResults2() {
 }
 function showNewResults2(results) {
 
-//alert(results);
 	results = $.parseJSON(results);
 
 	for (var i = 0; i < results.length; i++) {
 		if (results[i].hasOwnProperty('productId')) {
-			//alert(results[i]['categoryId']);
 			$('#productId'+i).data('id' , results[i]['productId']);
 			$('#productId'+i).text((results)[i]['productName']);
 			$('#productId'+i).show();
@@ -137,7 +133,6 @@ function showNewResults2(results) {
 			break;
 	}
 }
-
 //user is "finished typing," do something
 function doneTyping2 () {
     var data;
@@ -170,8 +165,8 @@ function printProductInfo(results) {
 $('.productliclass').click(function(event) {
 	var text = $(event.target).data('id');
 	$('#productId').data('id',text);
-	//alert($('#categoryId').data('id'));
 	$('#productId').val($(event.target).text());
+    $('.productliclass').hide();
     $.ajax({url: "/backend/productInfo.php", 
         data: {productId : text}, 
         success: function(result){
